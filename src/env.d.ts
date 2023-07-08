0b75f58288890b9f855399a45aa682d2734778e0
antheiz <salupacode@gmail.com>
2023-07-08 21:45:41 +0700

    fix: update env config

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_CUSTOM_ENV_VARIABLE: string;
   readonly PUBLIC_SUPABASE_URL: string;
   readonly PUBLIC_SUPABASE_ANON_KEY: string;
-  readonly PUBLIC_VERCEL_ENV: string;
 }
 
 interface ImportMeta {
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,16 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const env = import.meta.env.PUBLIC_VERCEL_ENV;
-let supabaseUrl: string | undefined;
-let supabaseAnonKey: string | undefined;
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-if (env === "development") {
-  supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-  supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
-} else {
-  supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
-  supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
-}
-
-export const supabase = createClient<Database>(supabaseUrl!, supabaseAnonKey!);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);

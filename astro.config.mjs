e16210aa975fe2f13424428e37db140c24166d40
antheiz <salupacode@gmail.com>
2023-08-22 13:02:05 +0700

    chore: set base site url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+    site: "https://kamus.antroytech.my.id/",
+    integrations: [tailwind(), alpinejs()]
 });

6121de9207beda49e602c0354b6802678a6051ce
antheiz <salupacode@gmail.com>
2023-10-03 09:46:16 +0700

    fix: delete baseURL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://antroy-tech.github.io/kastau-terjemahan",
 	integrations: [starlight({
 		title: 'Kastau Terjemahan',
 		logo: {

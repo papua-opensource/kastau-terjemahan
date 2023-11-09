3da961cf8ed6102a237755bbb045fe4f540f2762
karokari <144463414+karokari@users.noreply.github.com>
2023-11-09 09:17:05 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://antroy-tech.github.io",
+	site: "https://docs.kastauterjemahan.web.id",
 	base: "/kastau-terjemahan",
 	integrations: [starlight({
 		title: 'Kastau Terjemahan',
